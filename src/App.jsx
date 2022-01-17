bf758b009058374889a564909cd188ad520ba817
maheshvaitla <maheshvaitla@gmail.com>
2022-01-17 15:28:54 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Admin } from './Components/Admin';
+import { Dasboard } from './Components/Dasboard';
+import { Login } from './Components/Login';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/login" element={<Login />} ></Route>
+        <Route path="/admin" element={<Admin />} ></Route>
+        <Route path="/dashbaord" element={<Dasboard />} ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Dasboard.jsx
@@ -0,0 +1,5 @@
+export const Dasboard =()=>{
+    return <div>
+        
+    </div>
+}
